715d365aab0dd5ce93d559888792e62a24e54223
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-03-26 23:12:54 +0530

    add html n css

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hi")
+let tl = gsap.timeline();
+
+tl.to("#page1", {
+  y: "100vh",
+  duration: 0,
+  scale: 0.6,
+});
+tl.to("#page1", {
+  y: "30vh",
+  duration: 1,
+  delay: 1,
+});
+tl.to("#page1", {
+  rotate: -360,
+  duration: 0.8,
+  scale: 1,
+  y: "0vh",
+});
