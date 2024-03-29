02689047856110eed1ecde824bc939177610bfdf
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-03-29 16:06:37 +0530

    page-9 added

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let tl = gsap.timeline();
+/*let tl = gsap.timeline();
 tl.to("#main", {
   y: "100vh",
   duration: 0,
-  scale: 0.4
+  scale: 0.4,
 });
 tl.to("#main", {
   y: "-200vh",
   duration: 1,
-  delay: 1
+  delay: 1,
 });
 tl.to("#main", {
   rotate: -360,
   duration: 1,
-  delay:1.3,
+  delay: 1.3,
   scale: 1,
-  y: "0vh"
-});
+  y: "0vh",
+});*/
